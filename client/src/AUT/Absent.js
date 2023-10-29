0a04b6b62f0204d6bb6ea88b9e19df72583717f6
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2023-10-29 16:36:26 -0400

    Implemented idea input, previous ideas display, delete features

--- client/src/AUT/Absent.js
+++ client/src/AUT/Absent.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
+let nextId = 0;
+
 const Absent = () => {
-    const ideas = [];
+    const [input, setInput] = useState(''); // store currently inputted idea in input form
+    const [ideas, setIdeas] = useState([]); // previously entered ideas
+
+    // const handleChange = e => { // e is short for event
+    //     setInput(e.target.value);
+    // }
+
+    // const handleSubmit = e => { // e is short for event
+    //     e.preventDefault(); // prevents page from refreshing upon clicking submit
+    //     setInput(''); // clears the input form
+    // }
 
     return (
     <div className="h-screen w-screen items-center justify-center flex text-3xl font-semibold space-y-8 p-14 bg-amber-400">
@@ -14,13 +26,42 @@ const Absent = () => {
                     <div className="w-full h-96 rounded-[60px] bg-orange-500"></div>
                     <h1 className="mt-4">Item</h1>
                 </div>
+
                 <div className="w-1/3">
-                    <h2 className="mb-4">Enter Ideas below</h2>
+                    <h2 className="mb-4">Enter Alternative Uses below</h2>
+
                     <div className="flex flex-row space-x-4 justify-between">
-                        <input className="w-80"/>
-                        <button className="outline outline-offset-2 outline-2 text-xl px-2">Submit</button>
+                        <input className="w-max p-2 rounded-md" 
+                            value={input}
+                            onChange={e => setInput(e.target.value)}/>
+                        <button className="outline outline-offset-2 outline-2 rounded-md text-xl px-2"
+                            onClick={() => { 
+                                setIdeas([
+                                  ...ideas,
+                                  { id: nextId++, name: input }
+                                ]);
+                                setInput('');
+                              }}>Submit</button>
                     </div>
-                    <div className="w-full h-3/4 bg-orange-500 mt-4"></div>
+                    
+                    <div className="h-3/4 bg-orange-500 mt-4 rounded-lg p-4 grid place-items-start">
+                    <ul className="object-left flex flex-wrap">
+                        {ideas.map(idea => (
+                        <li key={idea.id} className="text-left flex justify-between">
+                            <span>{`\u2022 ${idea.name}`}{' '}</span>
+                            <button onClick={() => {
+                                setIdeas(
+                                    ideas.filter(a =>
+                                    a.id !== idea.id
+                                    )
+                                );
+                                }}>
+                                ❌
+                            </button>
+                        </li>
+                        ))}
+                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
